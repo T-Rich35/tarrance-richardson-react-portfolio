03e80db98e95c6aa285877e86b0405155372b357
Tarrance <richistee@gmail.com>
2022-09-06 20:48:12 -0500

    nav-bar

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,20 +8,26 @@ export default class NavigationComponent extends Component {
 
     render() {
         return (
-            <div>
-                <NavLink exact to="/">
-                    Home
-                </NavLink>
+            <div className="nav-wrapper">
+                    <div className="left-name">   
+                    <NavLink exact to="/">
+                        Home
+                    </NavLink>
 
-                <NavLink to="/about-me">
-                    About
-                </NavLink>
+                    <NavLink to="/about-me">
+                        About
+                    </NavLink>
 
-                <NavLink to="/about-me">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/blog">Blog</NavLink>
+                    <NavLink to="/about-me">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/blog">Blog</NavLink>
 
-                {false?<button>Add Blog</button> : ''}
+                    {false?<button>Add Blog</button> : ''}
+                </div> 
+
+                <div className="right-side">
+                    TARRANCE RICHARDSON
+                </div> 
             </div>
         );
     }
