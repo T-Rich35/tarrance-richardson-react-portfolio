0886f9ca6bf2bab0906f2b8785a010409af62162
Tarrance <richistee@gmail.com>
2022-09-21 06:59:19 -0500

    add HandleDeleteClick

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -14,6 +14,21 @@ export default class PortfolioManager extends Component {
 
         this.handleSuccessFormSubmission = this.handleSuccessFormSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+
+    handleDeleteClick(portfolioItem) {
+        axios.delete(`https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, {withCredentials: true})
+        .then(response => {
+            this.setState({
+                portfolioItems: this.state.portfolioItems.filter(item => {
+                    return item.id !== portfolioItem.id;
+                })
+            })
+            return response.data;
+        }).catch(error => {
+            console.log('handleDeleteClick error', error);
+        }); 
     }
 
     handleSuccessFormSubmission(portfolioItem) {
@@ -54,7 +69,9 @@ export default class PortfolioManager extends Component {
                 </div>
 
                 <div className='right-column'>
-                <PortfolioSidebarList data={this.state.portfolioItems}/>
+                <PortfolioSidebarList 
+                  handleDeleteClick={this.handleDeleteClick}  
+                  data={this.state.portfolioItems}/>
                 </div>
                 
 
