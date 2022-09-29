4fa09a389d56bcde9e537bd5a23a96a0919169bb
Tarrance <richistee@gmail.com>
2022-09-28 20:19:40 -0500

    add blog-form function

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -11,14 +11,27 @@ export default class RichTextEditor extends Component {
     this.state = {
       editorState: EditorState.createEmpty(),
     };
+
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
   }
+
+  onEditorStateChange(editorState) {
+    this.setState(
+      { editorState },
+      this.props.handleRichTextEditorChange(
+        draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+      )
+    );
+  }
+
   render() {
     return (
       <div>
         <Editor
-          editorState={this.state.EditorState}
+          editorState={this.state.editorState}
           wrapperClassName="demo-wrapper"
           editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
         />
       </div>
     );
