9a63fd5d07bd573c6eb8dfec0b28f5b6d0a8741a
Tarrance <richistee@gmail.com>
2022-10-04 14:50:18 -0500

    portfolio detail styles

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import PortfolioContainer from '../portfolio/portfolio-container';
+import React from "react";
 
-export default function() {
-     return (
-            <div>
-                <PortfolioContainer />
+import PortfolioContainer from "../portfolio/portfolio-container";
 
-            </div>
-        );
-    
+export default function () {
+  return (
+    <div>
+      <PortfolioContainer />
+    </div>
+  );
 }
